74bc1b9f372df9ad29eb0bec4d73f51d98ef133d
Viktor Korolyuk <8334048+viktorKorolyuk@users.noreply.github.com>
2022-03-10 20:25:22 -0500

    Create Protocol.java

---
+++ Protocol.java
@@ -0,0 +1,8 @@
+/**
+ * Defines the DateProtocol for the networking assignment
+ */
+public class Protocol {
+  public static final String HEADER = "DSRV";
+  public static final int FLAG_DATE = 0b01;
+  public static final int FLAG_TIME = 0b10;
+}
